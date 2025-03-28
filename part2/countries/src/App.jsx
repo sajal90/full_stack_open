88aa60320e796ecaa70412905d868b1fa9a77866
sajal90 <sajal2005gupta@gmail.com>
2025-03-28 18:29:21 +0530

    countries 2

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -18,7 +18,20 @@ const CountryInfo = ({ country }) => {
   );
 };
 
-const CountriesList = ({ countries }) => {
+const CountryElement = ({ country, showElement }) => {
+  return (
+    <div>
+      <li>
+        {country.name.common}
+        <button type="button" onClick={() => showElement(country.name.common)}>
+          show
+        </button>
+      </li>
+    </div>
+  );
+};
+
+const CountriesList = ({ countries, showElement }) => {
   if (countries.length === 1) {
     return <CountryInfo country={countries[0]} />;
   }
@@ -28,7 +41,13 @@ const CountriesList = ({ countries }) => {
   }
   return (
     <div>
-      {countries.map((c) => <li key={c.name.common}>{c.name.common}</li>)}
+      {countries.map((c) => (
+        <CountryElement
+          key={c.name.common}
+          country={c}
+          showElement={showElement}
+        />
+      ))}
     </div>
   );
 };
@@ -56,12 +75,20 @@ const App = () => {
     setSearch(event.target.value);
   };
 
+  const showElement = (name) => {
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+      .then((res) => {
+        setCountriesToShow([res.data]);
+      });
+  };
+
   return (
     <div>
       <p>
         find country <input value={search} onChange={handleChange} />
       </p>
-      <CountriesList countries={countriesToShow} />
+      <CountriesList countries={countriesToShow} showElement={showElement} />
     </div>
   );
 };
