4829a1bcdf3a64f85936e9063669e3a424a9500c
sajal90 <sajal2005gupta@gmail.com>
2025-03-29 11:56:42 +0530

    phonebookback 4

--- part3/phonebookback/index.js
+++ part3/phonebookback/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-const persons = [
+let persons = [
 	{
 		"id": "1",
 		"name": "Arto Hellas",
@@ -48,6 +48,13 @@ app.get("/api/persons/:id", (request, response) => {
 	} else {
 		response.status(404).end();
 	}
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+	const id = request.params.id;
+	persons = persons.filter((p) => p.id != id);
+
+	response.status(204).end();
 });
 
 const PORT = 3001;
