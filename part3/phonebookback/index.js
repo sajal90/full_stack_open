ed7326c122eefc5a18e0ed6aa7fb6b5c7637ec7f
sajal90 <sajal2005gupta@gmail.com>
2025-03-29 13:25:02 +0530

    phonebookback 7,8

--- part3/phonebookback/index.js
+++ part3/phonebookback/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+
+morgan.token("data", (request, response) => {
+	if (request.method === "POST") {
+		return `${JSON.stringify(request.body)}`;
+	} else {
+		return " ";
+	}
+});
+
+app.use(
+	morgan(
+		`:method :url :status :res[content-length] - :response-time ms :data`,
+	),
+);
 
 let persons = [
 	{
