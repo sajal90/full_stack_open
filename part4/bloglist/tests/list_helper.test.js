37c711f408a610562a9916a24268988aa0543ab8
sajal90 <sajal2005gupta@gmail.com>
2025-04-07 22:38:08 +0530

    4.3

---
+++ part4/bloglist/tests/list_helper.test.js
@@ -0,0 +1,13 @@
+const { test, describe } = require("node:test");
+const assert = require("node:assert");
+
+const dummy = require("../utils/list_helper.js").dummy;
+
+describe("dummy", () => {
+	test("dummy returns one", () => {
+		const blogs = [];
+		const result = dummy(blogs);
+
+		assert.strictEqual(result, 1);
+	});
+});
