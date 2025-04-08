1dad5b8907406931eee0caccdab8ef41e1dd31d8
sajal90 <sajal2005gupta@gmail.com>
2025-04-08 19:11:10 +0530

    4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,4 +2,8 @@ const dummy = (blogs) => {
 	return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+	return blogs.reduce((s, b) => s + b.likes, 0);
+};
+
+module.exports = { dummy, totalLikes };
