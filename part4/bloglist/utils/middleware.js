40e7a8e610371f16def6acf45b4097deb8200d21
sajal90 <sajal2005gupta@gmail.com>
2025-04-18 23:14:48 +0530

    4.23

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -14,6 +14,25 @@ const unknownEndpoint = (request, response) => {
 	response.status(404).end();
 };
 
+const errorHandler = (error, request, response, next) => {
+	logger.error(error);
+
+	if (error.name === "ValidationError" || error.name === "TypeError") {
+		response.status(400).send({ error: "Content missing" });
+	} else if (
+		error.name === "MongoServerError" &&
+		error.message.includes("E11000 duplicate key error")
+	) {
+		return response.status(400).json({
+			error: "expected `username` to be unique",
+		});
+	} else if (error.name === "JsonWebTokenError") {
+		return response.status(401).json({ error: "invalid token" });
+	}
+
+	next(error);
+};
+
 const tokenExtractor = (request, response, next) => {
 	const auth = request.get("authorization");
 	if (auth && auth.startsWith("Beaver ")) {
@@ -28,27 +47,10 @@ const userExtractor = async (request, response, next) => {
 		if (decodedToken.id) {
 			request.user = await User.findById(decodedToken.id);
 		}
+	} else if (request.method === "POST" || request.method === "DELETE") {
+		throw jwt.JsonWebTokenError;
 	}
 	next();
-};
-
-const errorHandler = (error, request, response, next) => {
-	logger.error(error);
-
-	if (error.name === "ValidationError") {
-		response.status(400).send({ error: "Content missing" });
-	} else if (
-		error.name === "MongoServerError" &&
-		error.message.includes("E11000 duplicate key error")
-	) {
-		return response.status(400).json({
-			error: "expected `username` to be unique",
-		});
-	} else if (error.name === "JsonWebTokenError") {
-		return response.status(401).json({ error: "invalid token" });
-	}
-
-	next(error);
 };
 
 module.exports = {
