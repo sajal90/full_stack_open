15a4d6e40d8deda53a2d607c2fba9b9a3137ab77
sajal90 <sajal2005gupta@gmail.com>
2025-05-03 14:03:58 +0530

    5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,47 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
 
-export default Blog
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleVisible = () => {
+    setVisible(!visible);
+  };
+
+  if (!visible) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button type="button" onClick={toggleVisible}>view</button>
+      </div>
+    );
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button type="button" onClick={toggleVisible}>hide</button>
+      </div>
+      <div>
+        {blog.url}
+      </div>
+      <div>
+        likes {blog.likes}
+        <button type="button">like</button>
+      </div>
+      <div>
+        {blog.author}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
+
