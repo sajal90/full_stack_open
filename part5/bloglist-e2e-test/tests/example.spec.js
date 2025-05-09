994873794ecad1ea82545ae0446a4d39d96dacaa
sajal90 <sajal2005gupta@gmail.com>
2025-05-09 23:18:45 +0530

    5.17

---
+++ part5/bloglist-e2e-test/tests/example.spec.js
@@ -0,0 +1,15 @@
+const { test, expect, beforeEach, describe } = require("@playwright/test");
+
+describe("Blog app", () => {
+  beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:5173");
+  });
+
+  test("Login form is shown", async ({ page }) => {
+    await expect(
+      page.getByText(
+        "log in to application",
+      ),
+    ).toBeVisible();
+  });
+});
