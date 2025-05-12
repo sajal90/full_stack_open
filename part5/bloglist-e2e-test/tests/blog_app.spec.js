4bd8d4777df34d6259f3ed7b13be10f27a2d1830
sajal90 <sajal2005gupta@gmail.com>
2025-05-12 20:09:41 +0530

    5.22

--- part5/bloglist-e2e-test/tests/blog_app.spec.js
+++ part5/bloglist-e2e-test/tests/blog_app.spec.js
@@ -12,6 +12,14 @@ describe("Blog app", () => {
       },
     });
 
+    await request.post("http://localhost:3001/api/users", {
+      data: {
+        name: "jane",
+        username: "janee",
+        password: "password",
+      },
+    });
+
     await page.goto("http://localhost:5173");
   });
 
@@ -68,6 +76,15 @@ describe("Blog app", () => {
 
       await expect(page.getByText("archlinux best distro sajal")).not
         .toBeVisible();
+    });
+
+    test("only creator can see the remove btn", async ({ page }) => {
+      await page.getByRole("button", { name: "logout" }).click();
+      await page.goto("http://localhost:5173");
+      await loginWith(page, "janee", "password");
+
+      await page.getByRole("button", { name: "view" }).click();
+      await expect(page.getByText("remove")).not.toBeVisible();
     });
   });
 });
