c5d5c85619f039cb0e77c2fd722b092a4abfc52a
sajal90 <sajal2005gupta@gmail.com>
2025-06-03 23:04:00 +0530

    6.14

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
 import AnecdoteList from "./components/AnecdoteList.jsx";
 import AnecdoteForm from "./components/AnecdoteForm.jsx";
 import Filter from "./components/Filter.jsx";
 import Notification from "./components/Notification.jsx";
+import anecdoteService from "./services/anecdotes.js";
+import { setAnecdotes } from "./reducers/anecdoteReducer.js";
+import { useDispatch } from "react-redux";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    anecdoteService.getAll().then((anecs) => {
+      dispatch(setAnecdotes(anecs));
+    });
+  }, []);
+
   return (
     <div>
       <h2>Anecdotes</h2>
--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,7 +1,13 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
+import anecdoteService from "./services/anecdotes.js";
+import { setAnecdotes } from "./reducers/anecdoteReducer.js";
 import store from "./store.js";
+
+anecdoteService.getAll().then((anecs) => {
+  store.dispatch(setAnecdotes(anecs));
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
