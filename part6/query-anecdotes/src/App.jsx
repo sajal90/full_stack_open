326d81578b8bdcbd27a23d5ea1dc47cfb2f4e225
sajal90 <sajal2005gupta@gmail.com>
2025-06-12 18:57:18 +0530

    6.20

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,54 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import AnecdoteForm from "./components/AnecdoteForm.jsx";
+import Notification from "./components/Notification.jsx";
+
+const App = () => {
+  const handleVote = (anecdote) => {
+    console.log("vote");
+  };
+
+  const result = useQuery(
+    {
+      queryKey: ["anecdotes"],
+      queryFn: () =>
+        axios.get("http://localhost:3001/anecdotes").then((res) => res.data),
+      retry: 1,
+    },
+  );
+
+  if (result.isError) {
+    return (
+      <span>Anecdote service not available due to problems in the server</span>
+    );
+  }
+
+  if (result.isLoading) {
+    return <div>Loading....</div>;
+  }
+
+  const anecdotes = result.data;
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
